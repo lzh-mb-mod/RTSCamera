4d56b9201a4c62973bf2511cdbe68a814e72bb1e
Li Zhenhuan <lizhenhuan1019@qq.com>
2021-02-01 11:43:54 +0800

    Fix bugs

---
+++ source/RTSCamera/src/Patch/Fix/Patch_TeamAIComponent.cs
@@ -0,0 +1,15 @@
+﻿using TaleWorlds.MountAndBlade;
+
+namespace RTSCamera.Patch.Fix
+{
+    public class Patch_TeamAIComponent
+    {
+        public static bool TickOccasionally_Prefix(TacticComponent ____currentTactic)
+        {
+            if (____currentTactic == null)
+                return false;
+
+            return true;
+        }
+    }
+}
