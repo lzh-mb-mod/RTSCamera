b38ea83434dd68ec371cefeef56d11b905a8d3ba
Lkoinw <lkoinw@hotmail.fr>
2022-08-24 15:14:20 +0200

    Updated check for RBM and troop cards clickable again (#6)
    * Updated check for RBM 3.0.0
    
    * Make troop cards clickable

--- source/RTSCamera.CommandSystem/src/CommandSystemSubModule.cs
+++ source/RTSCamera.CommandSystem/src/CommandSystemSubModule.cs
@@ -26,8 +26,14 @@ namespace RTSCamera.CommandSystem
 
             Utility.ShouldDisplayMessage = true;
             Initialize();
+
+            // If RBM is loaded, disable the ChargeToFormation feature for infantry to not break RBM frontline behavior
             EnableChargeToFormationForInfantry =
-                TaleWorlds.Engine.Utilities.GetModulesNames().Select(ModuleHelper.GetModuleInfo).FirstOrDefault(info => info.Id == "RealisticBattleAiModule") == null;
+                TaleWorlds.Engine.Utilities.GetModulesNames().Select(ModuleHelper.GetModuleInfo).FirstOrDefault(info =>
+                info.Id == "RBM") == null
+            &&
+                TaleWorlds.Engine.Utilities.GetModulesNames().Select(ModuleHelper.GetModuleInfo).FirstOrDefault(info =>
+                info.Id == "RealisticBattleAiModule") == null;
             Module.CurrentModule.GlobalTextManager.LoadGameTexts(ModuleHelper.GetXmlPath(ModuleId, "module_strings"));
         }
 
