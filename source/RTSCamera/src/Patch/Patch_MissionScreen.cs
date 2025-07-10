30f083959e84796b04e620a78edde93cc999241f
Zhenhuan Li <lizhenhuan1019@outlook.com>
2025-07-11 02:31:48 +0800

    Add back MissionScreen patch to fix camera issue when Default to Free Camera is set to always.

---
+++ source/RTSCamera/src/Patch/Patch_MissionScreen.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using MissionSharedLibrary.Utilities;
+using System;
+using System.Reflection;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade.GauntletUI.Mission.Singleplayer;
+using TaleWorlds.MountAndBlade.View.Screens;
+using TaleWorlds.MountAndBlade.ViewModelCollection;
+
+namespace RTSCamera.Patch
+{
+    public class Patch_MissionScreen
+    {
+        private static bool _patched;
+        public static bool Patch(Harmony harmony)
+        {
+            try
+            {
+                if (_patched)
+                    return false;
+                _patched = true;
+
+                harmony.Patch(
+                    typeof(MissionScreen).GetMethod("OnMissionModeChange",
+                        BindingFlags.Instance | BindingFlags.NonPublic),
+                    postfix: new HarmonyMethod(typeof(Patch_MissionScreen).GetMethod(
+                        nameof(Prefix_OnMissionModeChange), BindingFlags.Static | BindingFlags.Public)));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Utility.DisplayMessage(e.ToString());
+                return false;
+            }
+
+            return true;
+        }
+        public static bool Prefix_OnMissionModeChange(MissionScreen __instance, MissionMode oldMissionMode, bool atStart)
+        {
+            if (__instance.Mission.Mode == MissionMode.Battle && oldMissionMode == MissionMode.Deployment)
+            {
+                Utility.SmoothMoveToAgent(__instance, true);
+                //Utility.SetIsPlayerAgentAdded(__instance, false);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
