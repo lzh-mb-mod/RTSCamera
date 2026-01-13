349f9308d671bcf25cad9bc25a35e45c8b939e38
LiZhenhuan1019 <lizhenhuan1019@outlook.com>
2026-01-13 16:31:06 +0800

    Sort hero list in option

--- source/RTSCamera/src/View/WatchAgentSelectionData.cs
+++ source/RTSCamera/src/View/WatchAgentSelectionData.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using MissionSharedLibrary.Utilities;
+using System.Linq;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.MountAndBlade.View.Screens;
 
@@ -12,6 +13,19 @@ namespace RTSCamera.View
         public WatchAgentSelectionData(MissionScreen missionScreen)
         {
             var agents = (Mission.Current.PlayerTeam?.ActiveAgents ?? Mission.Current.Agents).Where(agent => agent.Character != null && agent.IsHero).ToList();
+            agents.Sort((agent1, agent2) =>
+            {
+                var isHigherInMemberRoster = Utility.IsHigherInMemberRoster(agent1, agent2);
+                if (isHigherInMemberRoster == true)
+                {
+                    return -1;
+                }
+                if (isHigherInMemberRoster == false)
+                {
+                    return 1;
+                }
+                return agent1.Index - agent2.Index;
+            });
             SelectionOptionData = new MissionSharedLibrary.View.ViewModelCollection.Options.Selection.SelectionOptionData(i =>
             {
                 if (Mission.Current?.Mode == TaleWorlds.Core.MissionMode.Deployment)
